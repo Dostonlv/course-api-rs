95fadb4dcee50af1fde94c32627eabdb218fbfaa
dostonlv <dostonlv@icloud.com>
2026-06-14 21:14:38 +0500

    database done

--- src/db/instructors.rs
+++ src/db/instructors.rs
@@ -1,37 +1,67 @@
 use sqlx::{Error, PgPool};
 
-
 use crate::entities::{
+    instructors::{CreateInstructor, Instructor, UpdateInstructor},
     repository::Repository,
-    instructors::{CreateInstructor,Instructor, UpdateInstructor}
 };
 
 pub struct InstructorRepo;
 
 impl Repository for InstructorRepo {
-    type Model= Instructor;
-
-    type CreateDto=CreateInstructor;
-
-    type UpdateDto=UpdateInstructor;
+    type Model = Instructor;
+    type CreateDto = CreateInstructor;
+    type UpdateDto = UpdateInstructor;
 
     async fn create(pool: &PgPool, dto: &Self::CreateDto) -> Result<i64, Error> {
-        todo!()
+        sqlx::query_scalar!(
+            r#"INSERT INTO instructors (full_name, email, expertise)
+            VALUES ($1, $2, $3) RETURNING id"#,
+            dto.full_name,
+            dto.email,
+            dto.expertise
+        )
+        .fetch_one(pool)
+        .await
     }
 
     async fn get_by_id(pool: &PgPool, id: i64) -> Result<Option<Self::Model>, Error> {
-        todo!()
+        sqlx::query_as!(
+            Instructor,
+            r#"SELECT id, full_name, email, expertise 
+            FROM instructors WHERE id = $1"#,
+            id
+        )
+        .fetch_optional(pool)
+        .await
     }
 
     async fn get_all(pool: &PgPool) -> Result<Vec<Self::Model>, Error> {
-        todo!()
+        sqlx::query_as!(
+            Instructor,
+            r#"SELECT id, full_name, email, expertise 
+            FROM instructors"#
+        )
+        .fetch_all(pool)
+        .await
     }
 
     async fn update(pool: &PgPool, id: i64, dto: &Self::UpdateDto) -> Result<i64, Error> {
-        todo!()
+        sqlx::query_scalar!(
+            r#"UPDATE instructors
+            SET full_name = $1, email = $2, expertise = $3
+            WHERE id = $4 RETURNING id"#,
+            dto.full_name,
+            dto.email,
+            dto.expertise,
+            id
+        )
+        .fetch_one(pool)
+        .await
     }
 
     async fn delete(pool: &PgPool, id: i64) -> Result<Option<i64>, Error> {
-        todo!()
+        sqlx::query_scalar!(r#"DELETE FROM instructors WHERE id = $1 RETURNING id"#, id)
+            .fetch_optional(pool)
+            .await
     }
 }
--- src/db/students.rs
+++ src/db/students.rs
@@ -9,28 +9,57 @@ pub struct StudentRepo;
 
 impl Repository for StudentRepo {
     type Model = Student;
-
     type CreateDto = CreateStudent;
-
     type UpdateDto = UpdateStudent;
 
     async fn create(pool: &PgPool, dto: &Self::CreateDto) -> Result<i64, Error> {
-        todo!()
+        sqlx::query_scalar!(
+            r#"INSERT INTO students (full_name, email)
+            VALUES ($1, $2) RETURNING id"#,
+            dto.full_name,
+            dto.email
+        )
+        .fetch_one(pool)
+        .await
     }
 
     async fn get_by_id(pool: &PgPool, id: i64) -> Result<Option<Self::Model>, Error> {
-        todo!()
+        sqlx::query_as!(
+            Student,
+            r#"SELECT id, full_name, email, created_at
+            FROM students WHERE id = $1"#,
+            id
+        )
+        .fetch_optional(pool)
+        .await
     }
 
     async fn get_all(pool: &PgPool) -> Result<Vec<Self::Model>, Error> {
-        todo!()
+        sqlx::query_as!(
+            Student,
+            r#"SELECT id, full_name, email, created_at
+            FROM students"#
+        )
+        .fetch_all(pool)
+        .await
     }
 
     async fn update(pool: &PgPool, id: i64, dto: &Self::UpdateDto) -> Result<i64, Error> {
-        todo!()
+        sqlx::query_scalar!(
+            r#"UPDATE students
+            SET full_name = $1, email = $2
+            WHERE id = $3 RETURNING id"#,
+            dto.full_name,
+            dto.email,
+            id
+        )
+        .fetch_one(pool)
+        .await
     }
 
     async fn delete(pool: &PgPool, id: i64) -> Result<Option<i64>, Error> {
-        todo!()
+        sqlx::query_scalar!(r#"DELETE FROM students WHERE id = $1 RETURNING id"#, id)
+            .fetch_optional(pool)
+            .await
     }
 }
